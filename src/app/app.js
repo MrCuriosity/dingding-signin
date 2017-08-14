32cb334739a92294788282540790edc4ddb73ec8
wangchao <13982205298@163.com>
2017-08-14 20:38:22 +0800

    wrap app init code in a try catch to locate err

--- src/app/app.js
+++ src/app/app.js
@@ -12,47 +12,53 @@ import './app.less';
 const { Toast, Dialog } = window.SaltUI;
 const customHistory = hashHistory;
 
-if (isDev && window.chrome && window.chrome.webstore) { // This is a Chrome only hack
-  // see https://github.com/livereload/livereload-extensions/issues/26
-  setInterval(() => {
-    document.body.focus();
-  }, 200);
-}
-
-// bind fastclick
-FastClick && FastClick.attach(document.body);
-
-// 这里使用setup来配置noflux
-setup('fn', {
-  toast: Toast,
-  dialog: Dialog,
-  DB,
-  history: customHistory,
-});
-
-const Loading = () => <div className="kuma-loading" />;
-const Empty = () => <div>暂无数据</div>;
-
-// 修改 LogicRender 增加默认配置
-// 用来自定义Loading和Empty的样式
-Object.assign(LogicRender.defaultProps, { Empty, Loading });
-
-class App extends Component {
-  render() {
-    return (
-      <div className={'appContainerStyle'}>
-        {this.props.children}
-      </div>
-    );
+try {
+  if (isDev && window.chrome && window.chrome.webstore) { // This is a Chrome only hack
+    // see https://github.com/livereload/livereload-extensions/issues/26
+    setInterval(() => {
+      document.body.focus();
+    }, 200);
   }
+
+  // bind fastclick
+  FastClick && FastClick.attach(document.body);
+
+  // 这里使用setup来配置noflux
+  setup('fn', {
+    toast: Toast,
+    dialog: Dialog,
+    DB,
+    history: customHistory,
+  });
+
+  const Loading = () => <div className="kuma-loading" />;
+  const Empty = () => <div>暂无数据</div>;
+
+  // 修改 LogicRender 增加默认配置
+  // 用来自定义Loading和Empty的样式
+  Object.assign(LogicRender.defaultProps, { Empty, Loading });
+
+  class App extends Component {
+    render() {
+      return (
+        <div className={'appContainerStyle'}>
+          {this.props.children}
+        </div>
+      );
+    }
+  }
+
+  render(
+    <Router history={customHistory}>
+      <Route name="app" path="/" component={App}>
+        <IndexRoute component={Signin} />
+        <Route path="signin" component={Signin} />
+      </Route>
+    </Router>,
+    document.getElementById('App'),
+  );
+} catch(e) {
+  alert(`App init error => ${JSON.stringify(e)}`)
 }
 
-render(
-  <Router history={customHistory}>
-    <Route name="app" path="/" component={App}>
-      <IndexRoute component={Signin} />
-      <Route path="signin" component={Signin} />
-    </Route>
-  </Router>,
-  document.getElementById('App'),
-);
+
