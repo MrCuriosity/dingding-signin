58096abcc8f3430c7a366ec86408ece4b45d34d7
wangchao <13982205298@163.com>
2017-08-15 14:05:01 +0800

    try error location

--- src/app/app.js
+++ src/app/app.js
@@ -32,34 +32,34 @@ try {
     history: customHistory
   });
 
-  const Loading = () => <div className="kuma-loading" />;
+  const Loading = () => <div className="kuma-loading"></div>;
   const Empty = () => <div>暂无数据</div>;
 
   // 修改 LogicRender 增加默认配置
   // 用来自定义Loading和Empty的样式
   Object.assign(LogicRender.defaultProps, { Empty, Loading });
 
-  class App extends Component {
-    render() {
-      return (
-        <div className='appContainerStyle'>
-          {this.props.children}
-        </div>
-      )
-    }
-  }
-
-  render(
-    <Router history={customHistory}>
-      <Route name="app" path="/" component={App}>
-        <IndexRoute component={Signin} />
-        <Route path="signin" component={Signin} />
-      </Route>
-    </Router>,
-    document.getElementById('App')
-  );
 } catch(e) {
   alert(`App init error => ${JSON.stringify(e)}`)
 }
+class App extends Component {
+  render() {
+    return (
+      <div className='appContainerStyle'>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+render(
+  <Router history={customHistory}>
+    <Route name="app" path="/" component={App}>
+      <IndexRoute component={Signin} />
+      <Route path="signin" component={Signin} />
+    </Route>
+  </Router>,
+  document.getElementById('App')
+);
 
 
