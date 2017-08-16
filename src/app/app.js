8554aa31373f08a690d2ebb93f3b051ea078f349
wangchao <13982205298@163.com>
2017-08-16 10:42:58 +0800

    rm alert

--- src/app/app.js
+++ src/app/app.js
@@ -12,35 +12,29 @@ import './app.less';
 const { Toast, Dialog } = window.SaltUI;
 const customHistory = hashHistory;
 
-try {
-
-  // if (isDev && window.chrome && window.chrome.webstore) { // This is a Chrome only hack
-  //   // see https://github.com/livereload/livereload-extensions/issues/26
-  //   setInterval(() => {
-  //     document.body.focus();
-  //   }, 200);
-  // }
-
-  // bind fastclick
-  FastClick && FastClick.attach(document.body);
-
-  // 这里使用setup来配置noflux
-  setup('fn', {
-    toast: Toast,
-    dialog: Dialog,
-    DB: DB,
-    history: customHistory
-  });
-
-  // 修改 LogicRender 增加默认配置
-  // 用来自定义Loading和Empty的样式
-  const Loading = () => <div className="kuma-loading"></div>;
-  const Empty = () => <div>暂无数据</div>;
-  (Object.assign && LogicRender.defaultProps) && Object.assign(LogicRender.defaultProps, { Empty: Empty, Loading: Loading });
-
-} catch(e) {
-  alert(`App init error => ${JSON.stringify(e)}`)
+if (isDev && window.chrome && window.chrome.webstore) { // This is a Chrome only hack
+  // see https://github.com/livereload/livereload-extensions/issues/26
+  setInterval(() => {
+    document.body.focus();
+  }, 200);
 }
+
+// bind fastclick
+FastClick && FastClick.attach(document.body);
+
+// 这里使用setup来配置noflux
+setup('fn', {
+  toast: Toast,
+  dialog: Dialog,
+  DB: DB,
+  history: customHistory
+});
+
+// 修改 LogicRender 增加默认配置
+// 用来自定义Loading和Empty的样式
+const Loading = () => <div className="kuma-loading"></div>;
+const Empty = () => <div>暂无数据</div>;
+(Object.assign && LogicRender.defaultProps) && Object.assign(LogicRender.defaultProps, { Empty: Empty, Loading: Loading });
 
 class App extends Component {
   render() {
